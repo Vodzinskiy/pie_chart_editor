7c319e3740db90ec9ece1defb29f9e4d850fb465
Vodzinskiy <roma.vodzinskiy@gmail.com>
2022-03-30 12:55:39 +0300

    add basic tableView functional

---
+++ src/main/java/ipz/coursework/pie_chart_editor/DataForPieChart.java
@@ -0,0 +1,30 @@
+package ipz.coursework.pie_chart_editor;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class DataForPieChart {
+    private SimpleStringProperty interest;
+    private SimpleStringProperty name;
+
+    public DataForPieChart(String interest, String name) {
+        this.interest = new SimpleStringProperty(interest);
+        this.name = new SimpleStringProperty(name);
+    }
+
+    public String getInterest() {
+        return interest.get();
+    }
+
+    public void setInterest(String interest) {
+        this.interest = new SimpleStringProperty(interest);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name = new SimpleStringProperty(name);
+    }
+}
--- src/main/java/ipz/coursework/pie_chart_editor/MainController.java
+++ src/main/java/ipz/coursework/pie_chart_editor/MainController.java
@@ -1,11 +1,16 @@
 package ipz.coursework.pie_chart_editor;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,33 +24,48 @@ public class MainController {
     private URL location;
 
     @FXML
-    private TableColumn<?, ?> color;
+    private TableColumn<DataForPieChart, Integer> color;
 
     @FXML
-    private TableColumn<?, ?> interest;
+    private TableColumn<DataForPieChart, String> interest;
 
     @FXML
-    private TableColumn<?, ?> name;
+    private TableColumn<DataForPieChart, String> name;
 
     @FXML
-    private TableView<?> tabelView;
+    private TableView<DataForPieChart> tableView;
 
-
+    private ObservableList<DataForPieChart> dataForPieChart = FXCollections.observableArrayList(
+            new DataForPieChart("0","Змінна1")
+    );
 
     @FXML
     void addToList(ActionEvent event) {
-
+        int count = tableView.getItems().size() + 1;
+        dataForPieChart.add(new DataForPieChart("0","Змінна"+count));
     }
 
     @FXML
     void removeFromList(ActionEvent event) {
+        tableView.getItems().removeAll(tableView.getSelectionModel().getSelectedItem());
+    }
 
+    @FXML
+    void clear(ActionEvent event) {
+        tableView.getItems().clear();
+        dataForPieChart.add(new DataForPieChart("0","Змінна1"));
     }
 
     @FXML
     void initialize() {
 
+        interest.setCellValueFactory(new PropertyValueFactory<>("interest"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
 
+        name.setCellFactory(TextFieldTableCell.forTableColumn());
+        interest.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        tableView.setItems(dataForPieChart);
     }
 
 }
