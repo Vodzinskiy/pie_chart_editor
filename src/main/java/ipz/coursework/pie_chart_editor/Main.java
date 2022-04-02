b78036fc47faf54f3516042b8f02fed7d7e39d0b
Arsen0109 <balasarseniy777@gmail.com>
2022-04-02 18:24:30 +0300

    set the icon

--- src/main/java/ipz/coursework/pie_chart_editor/Main.java
+++ src/main/java/ipz/coursework/pie_chart_editor/Main.java
@@ -3,6 +3,7 @@ package ipz.coursework.pie_chart_editor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +21,8 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("pie_chart_editor");
+        Image icon = new Image("file:icon.png");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
